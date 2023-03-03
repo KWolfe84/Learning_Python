9cff339abf061f371d3b264354ae1f3387600b87
Kenny <SparQz9@gmail.com>
2023-03-03 12:08:55 -0500

    Add files via upload
    This is just a simple script that asks what IP address you would like to scan.  It then scans with nmap based on which type of scan you choose, then asks you what kind of gobuster scan you'd like to run.  Finally it runs dirb and spits them all out into a .txt file labeled with the scanned IP address and an identifier on the scan type.

---
+++ betterscan.py
@@ -0,0 +1,48 @@
+import os
+import subprocess
+# Just a simple script to automate scan and enumeration 
+# Made by: SparQz
+
+# Get IP address from user
+ip_address = input("Enter the IP address to scan: \n")
+
+# Get type of Nmap scan to run from user
+
+scan_type = input("Enter the type of Nmap scan to run(quiet/aggressive/intensive): ")
+
+print ("-----Running Nmap scan-----\n")
+# Run Nmap scan
+if scan_type == "quiet":
+    nmap_command = f"nmap -sV -Pn {ip_address} -oN {ip_address}_nmap.txt"
+elif scan_type == "aggressive":
+    nmap_command = f"nmap -A {ip_address} -oN {ip_address}_nmap.txt"
+elif scan_type == "intensive":
+    nmap_command = f"nmap -sC -sV -A -T4 -v {ip_address} -oN {ip_address}_nmap.txt"
+else:
+    print("Invalid scan type")
+    exit()
+
+os.system(nmap_command)
+
+gobuster_type = input("Which Gobuster scan type would you like? (vhost/dir): \n")
+# Run Gobuster scan
+if gobuster_type == "dir":
+    gobuster_command = f"gobuster dir -u http://{ip_address} -w $DIRMEDIUM -o {ip_address}_dir_gobuster.txt"
+elif gobuster_type == "vhost":
+    gobuster_command = f"gobuster vhost -u http://{ip_address} -w /usr/share/payloads/SecLists/Discover/Web-Content/SVNDigger/all-dirs.txt -o {ip_address}_vhost_gobuster.txt"
+else:
+    print("Invalid scan type")
+os.system(gobuster_command)
+
+
+# Run dirb scan
+dirb_command = f"dirb http://{ip_address} /usr/share/payloads/SecLists/Discovery/Web-Content/SVNDigger/all-dirs.txt -o {ip_address}_dirb.txt"
+
+# Print output file paths
+print(f"Nmap output saved to {ip_address}_{scan_type}_nmap.txt")
+if gobuster_type == "dir":
+    print(f"Gobuster output saved to {ip_address}_dir_gobuster.txt")
+if gobuster_type == "vhost":
+    print(f"Gobuster output saved to {ip_address}_vhost_gobuster.txt")
+
+print(f"dirb output saved to {ip_address}_dirb.txt")
