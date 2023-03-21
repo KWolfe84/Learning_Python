7ba78f67495bf1b9ad0788f0e796409056b92e67
Kenny <SparQz9@gmail.com>
2023-03-21 12:51:20 -0400

    Add files via upload

--- betterscanv2.py
+++ betterscanv2.py
@@ -1,73 +1,71 @@
-import tkinkter as tk
-from tkinter import ttk
-import os
+import tkinter as tk
 import subprocess
-# Just a simple script to automate scan and enumeration 
-# Made by: SparQz
 
-# Create a Tkinter window and set its properties
-root = tk.Tk()
-root.title("Better Scan")
-root.geometry("400x200")
-root.resizable(False, False)
+def scan():
+	ip_address = ip_entry.get()
+	options = ""
+	if nmap_var.get():
+		options +="-sS "
+	if udp_scan_var.get():
+		options += "-sU"
+	if stealth_scan_var.get():
+		options += "-sS -sV -T4 -O -F --version-light "
+	if full_scan_var.get():
+		options += "-sS -sV -sC -A -O -T4 "
+	if os_fingerprint_var.get():
+		options += "-O "
+	if service_info_var.get():
+		options += "-sV "
+	subprocess.run(f"nmap {options} {ip_address}", shell=True)
+	if gobuster_var.get():
+		subprocess.run(f"gobuster dir -u http://{ip_address} -w /usr/share/payloads/SecLists/Discover/Web-Content/directory-list-2.3-medium.txt -x php, txt", shell=True)
+		
+	if dirb_var.get():
+		subprocess.run(f"dirb http://{ip_address}", shell=True)
 
-# Create a frame for the checkboxes
-frame = ttk.Frame(root)
-frame.pack(padx=10, pady=10)
+window = tk.Tk()
+window.title("Over9000")
 
-# Add the checkboxes
+# IP address entry
 
-quick_scan_var = tk.BooleanVar()
-quick_scan_cb = ttk.Checkbutton(frame, text="Quick Scan", variable=quick_scan_var)
+ip_label = tk.Label(window, text="Enter IP Address: ")
+ip_label.pack()
+ip_entry = tk.Entry(window)
+ip_entry.pack()
 
+
+nmap_var = tk.BooleanVar()
+nmap_check = tk.Checkbutton(window, text="nmap", variable=nmap_var)
+nmap_check.pack()
+udp_scan_var = tk.BooleanVar()
+udp_scan_check = tk.Checkbutton(window, text="UDP Scan", variable=udp_scan_var)
+udp_scan_check.pack()
+stealth_scan_var = tk.BooleanVar()
+stealth_scan_check = tk.Checkbutton(window, text="Stealth Scan", variable=stealth_scan_var)
+stealth_scan_check.pack()
 full_scan_var = tk.BooleanVar()
-full_scan_cb = ttk.Checkbutton(frame, text="Full Scan", variable=full_scan_var)
-full_scan_cb.pack(anchor=tk.W)
-
-dirb_frame= ttk.LabelFrame(root, text="Dirb")
-dirb_frame.pack(padx=10, pady=10, fill=tk.X)
-
-dirb_var= tk.BooleanVar()
-dirb_cb= ttk.Checkbutton(dirb_frame, text="Enable Dirb", variable=dirb_var)
-dirb_cb.pack(side=tk.LEFT)
-
-
-gobuster_frame = ttk.LabelFrame(root, text="Gobuster")
-gobuster_frame.pack(padx=10, pady=10, fill=tk.X)
-
-gobuster_var= tk.BooleanVar()
-gobuster_cb= ttk.Checkbutton(gobuster_frame, text="Enable Gobuster", variable=gobuster_var)
-gobuster.pack(side=tk.LEFT)
-
-dirb_options_frame = ttk.Frame(dirb_frame)
-dirb_options_frame.pack(fill=tk.X)
-
-gobuster_options_frame = ttk.Frame(gobuster_frame)
-gobuster_options_frame.pack(fill=tk.X)
-
-dirb_wordlist_var = tk.BooleanVar()
-dirb_wordlist_cb = ttk.Checkbutton(dirb_options_frame, text="Use Wordlist", variable=dirb_wordlist_var)
-dirb_wordlist_cb.pack(side=tk.LEFT)
-
-gobuster_wordlist_var = tk.BooleanVar()
-gobuster_wordlist_cb = ttk.Checkbutton(dirb_options_frame, text="Use Wordlist", variable=gobuster_wordlist_var)
-gobuster_wordlist_cb.pack(side=tk.LEFT)
+full_scan_check = tk.Checkbutton(window, text= "Full Scan", variable=full_scan_var)
+full_scan_check.pack()
+os_fingerprint_var = tk.BooleanVar()
+os_fingerprint_check = tk.Checkbutton(window, text="OS Fingerprint", variable=os_fingerprint_var)
+os_fingerprint_check.pack()
+service_info_var = tk.BooleanVar()
+service_info_check = tk.Checkbutton(window, text="Service Info", variable=service_info_var)
+service_info_check.pack()
+gobuster_var = tk.BooleanVar()
+gobuster_check = tk.Checkbutton(window, text="gobuster", variable=gobuster_var)
+gobuster_check.pack()
+dirb_var = tk.BooleanVar()
+dirb_check = tk.Checkbutton(window, text="dirb", variable=dirb_var)
+dirb_check.pack()
 
 
-def run_scan():
-    if quick_scan_var.get():
-        # Run Quick scan
-        
-        elif full_scan_var.get()
-        # Run Full Scan
-    else:
-        echo "Incorrect scan type selected."
-        
-# Add a button to start the scan
-scan_button =ttk.Button(root, text="Scan" , command=run_scan)
-scan_button.pack(pady=10)
+# Scan Button
+scan_button = tk.Button(window, text="Scan", command=scan)
+scan_button.pack()
 
-root.mainloop()
+window.mainloop()
 
 
+	
 
